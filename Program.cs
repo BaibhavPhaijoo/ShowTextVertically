717ff496e6f91de3ddbc4dac9c7d7d6f64cfcc06
BAIBHAV PHAIJOO <baibhavphaijoo@gmail.com>
2025-09-30 11:45:47 +0545

    add so that it asks how much delay time

--- Program.cs
+++ Program.cs
@@ -5,11 +5,23 @@ class Program {
         static void Main() {
             Console.WriteLine("Write some text: ");
             string text = Console.ReadLine();
+            Console.WriteLine("Write how much time delay to keep between showing characters in miliseconds(ms): ");
+            string input = Console.ReadLine();
+
+            int sleepTime;
+            if (!int.TryParse(input, out sleepTime)) {
+                Console.WriteLine("Invalid input! Using default delay of 500ms");
+                sleepTime = 500;
+            }
+
+
         
             for(int i = 0; i < text.Length; i++) {
                 Console.WriteLine(text[i]);
-                Thread.Sleep(350);
+                Thread.Sleep(sleepTime);
         }
         Console.ReadKey();
+        Console.WriteLine("!code execution success!");
     }
 }
+
